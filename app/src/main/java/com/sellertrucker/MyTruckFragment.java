7cfe58b8280e0a62a6b9f2ed05906d9723b2a4fa
한유덕 <Duck@han-yudeog-ui-MacBook-Pro.local>
2017-07-22 01:22:52 +0900

    home 진행중

---
+++ app/src/main/java/com/sellertrucker/MyTruckFragment.java
@@ -0,0 +1,22 @@
+package com.sellertrucker;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class MyTruckFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        final View view = inflater.inflate(R.layout.fragment_my_truck, container, false);
+
+
+        return view;
+    }
+
+
+}
+
